2421d37ef17252ee977ac59719ccb1f9fcc10588
Jean Cruypenynck <filaton@me.com>
2015-07-19 16:17:13 +0200

    Testing for Python 3.4

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,5 @@
-def multiply (a, b):
+def multiply(a, b):
 	return a * b
 
-def add (a, b):
+def add(a, b):
 	return a + b
--- test/test_functions.py
+++ test/test_functions.py
@@ -1,7 +1,11 @@
 from src.functions import multiply, add
 
-def test_multiply ():
+def test_multiply():
 	assert multiply(1,2) == 2
+	assert multiply(0,1) == 0
 
-def test_add ():
+def test_add():
 	assert add(1,1) == 2
+
+def test_python3():
+	assert False, print "Bad assert"
