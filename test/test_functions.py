71a692f9d2900b0b8e416760cbf1d3def180351b
Jean Cruypenynck <filaton@me.com>
2015-07-19 18:13:01 +0200

    Corrects PEP8

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,15 +1,18 @@
 import sys
 from src.functions import multiply, add
 
+
 def test_multiply():
-	assert multiply(1,2) == 2
-	assert multiply(0,1) == 0
+    assert multiply(1, 2) == 2
+    assert multiply(0, 1) == 0
+
 
 def test_add():
-	assert add(1,1) == 2
+    assert add(1, 1) == 2
+
 
 def test_python3():
-	if sys.version_info[0] == 3:
-		assert True
-	else:
-		assert True
+    if sys.version_info[0] == 3:
+        assert True
+    else:
+        assert True
